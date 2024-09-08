6955e119a4d1b339fd438ec38bba7fa1f042221b
Habib Ali Machpud <habib.machpud@gmail.com>
2024-09-08 14:57:08 +0700

    Init new module

---
+++ simple-blog/src/test/java/io/github/bluething/playground/spring/boot/simpleblog/SimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.boot.simpleblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
